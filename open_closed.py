e50b1b4d74c4b82f9386d01220dd83d8cd61c302
berna39 <kalemajoseph39@gmail.com>
2022-01-19 20:25:16 +0200

    Illustration context of open close principe

--- open_closed.py
+++ open_closed.py
@@ -1,1 +1,21 @@
-#illustration
+#illustration of the problem
+class CloudService:
+    def __init__(self, client):
+        pass
+
+    def authenticate(self, client):
+        if client == 'aws':
+            pass
+            #some code to authenticate against aws
+        elif client == 'azure':
+            pass
+            #some code to authenticate against azure
+    
+    def deploy(self, client):
+        if client == 'aws':
+            pass
+            #some code to deploy to aws
+        elif client == 'azure':
+            pass
+            #some code deploy to azure
+        
