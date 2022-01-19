18cd88137a563778643ba775b0bd49ccfb55b4c5
berna39 <kalemajoseph39@gmail.com>
2022-01-19 20:33:46 +0200

    First case

--- open_closed.py
+++ open_closed.py
@@ -1,21 +1,24 @@
 #illustration of the problem
 class CloudService:
     def __init__(self, client):
-        pass
+        self.client = client
 
-    def authenticate(self, client):
-        if client == 'aws':
-            pass
+    def authenticate(self):
+        if self.client == 'aws':
+            print('auth to aws')
             #some code to authenticate against aws
-        elif client == 'azure':
-            pass
+        elif self.client == 'azure':
+             print('auth to azure')
             #some code to authenticate against azure
     
-    def deploy(self, client):
-        if client == 'aws':
-            pass
+    def deploy(self):
+        if self.client == 'aws':
+             print('deploy to aws')
             #some code to deploy to aws
-        elif client == 'azure':
-            pass
+        elif self.client == 'azure':
+             print('deploy to azure')
             #some code deploy to azure
+
+service = CloudService('aws') #example of deploying 
+service.deploy()
         
