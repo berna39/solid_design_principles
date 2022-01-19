24e0d7056e992f3f077fccbe57d3e3016cf40105
berna39 <kalemajoseph39@gmail.com>
2022-01-19 21:59:52 +0200

    Problem posed

--- liskov.py
+++ liskov.py
@@ -1,4 +1,8 @@
-class KitchenAppliance:
+# a delivered class can assume the place of it's super class
+from time import clock_settime
+
+
+class KitchenAppliance():
     def on():
         pass
 
@@ -8,8 +12,24 @@ class KitchenAppliance:
     def set_temperature():
         pass
 
-class Taster(KitchenAppliance):
-    pass
+class Toaster(KitchenAppliance):
+    def on():
+        return 'toaster on'
+
+    def off():
+        return 'toaster off'
+
+    def set_temperature():
+        return 'toaster temperature changed'
+
+#given that the juicer doesn't have the set_temperature, we should refractor the code
+class Juicer(KitchenAppliance):
+    def on():
+        return 'toaster on'
+
+    def off():
+        return 'toaster off'
 
 
-k = KitchenAppliance()
+juicer = Juicer()
+print(help(juicer)) # the juicer shouldn't 
